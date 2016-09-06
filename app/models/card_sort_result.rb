de3a309ae5b9cc5ea3681b121ccc8ea6d3431805
Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>
2016-09-06 15:26:05 -0400

    serious revisions
    Signed-off-by: Shaun McKinnon <shaun.mckinnon@gshiftlabs.com>

--- app/models/card_sort_result.rb
+++ app/models/card_sort_result.rb
@@ -2,4 +2,30 @@ class CardSortResult < ApplicationRecord
 
   belongs_to :card_sort
 
+  def pretty
+    dom = walkit
+    return dom.flatten.compact.join("").html_safe
+  end
+
+  def walkit item = false, tree = false
+    item = item || result
+    tree = tree || []
+
+    tree << "<ul>"
+    item.each do |key, value|
+      if value.kind_of?(Hash)
+        tree << "<li>"
+        tree << key
+        walkit value, tree
+      elsif value.kind_of?(Array)
+        value.compact.each{ |v| tree << "<li>#{v}</li>" }
+      end
+      tree << "</li>"
+    end
+
+    tree << "</ul>"
+
+    return tree
+  end
+
 end
